304e3a2cefa347560a9ed47397b35d96f1f9bf99
Huegelkoenig <46036679+Huegelkoenig@users.noreply.github.com>
2019-04-18 20:55:17 +0200

    simple CanvasButton class

---
+++ CanvasButton.js
@@ -0,0 +1,18 @@
+class CanvasButton{
+  constructor(x,y,width,height,text){
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.text=text;
+
+  }
+
+  draw(){
+    ctx.fillStyle='white';
+    ctx.fillRect(this.x,this.y,this.width,this.height);
+    ctx.fillStyle = 'black';
+    ctx.font = "30px Verdana";
+    ctx.fillText(this.text,this.x,this.y+this.height);
+  }
+}
--- main.js
+++ main.js
@@ -2,11 +2,17 @@ var canvas =document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var rect=canvas.getBoundingClientRect();
 
-ctx.fillStyle='lightgrey';
+var CButtons=[];
+CButtons[0] = new CanvasButton(30,70,100,40,'test');
+
 function loop(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle='lightgrey';
   ctx.fillRect(0,0,canvas.width,canvas.height);
 
+  for (let CButton of CButtons){
+    CButton.draw();
+  }
 
 requestAnimationFrame(loop);
 }
