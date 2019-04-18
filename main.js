78ecaf95011129572f95772fec45643beceee5fc
Huegelkoenig <46036679+Huegelkoenig@users.noreply.github.com>
2019-04-18 20:45:32 +0200

    fill canvas background

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var canvas =document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var rect=getBoundingClientRect(canvas);
+var rect=canvas.getBoundingClientRect();
 
+ctx.fillStyle='lightgrey';
 function loop(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+
 
 requestAnimationFrame(loop);
 }
