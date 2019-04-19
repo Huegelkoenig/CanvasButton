b817c5b9124bb4ad49290611ec79b2be86eab47e
Huegelkoenig <46036679+Huegelkoenig@users.noreply.github.com>
2019-04-19 04:06:41 +0200

    clickability of a CanvasButton can now be removed via CButton.makeClickable() / .makeUnclickable()

--- CanvasButton.js
+++ CanvasButton.js
@@ -1,18 +1,62 @@
+'use strict'
 class CanvasButton{
-  constructor(x,y,width,height,text){
+  constructor(x,y,width,height,text,color,clickable){
     this.x=x;
     this.y=y;
     this.width=width;
     this.height=height;
     this.text=text;
-
+    this.color = color;
+    this.clickable = clickable;
+    this.clickHandler; //just a dummy to store te function to be able to remove the eventListener    
+    if (this.clickable){
+      this.makeClickable();
+    }
   }
 
+  makeClickable(){
+    console.log('made clickable')
+    this.clickable = true;
+    this.clickHandler = this.clicker.bind(this)
+    canvas.addEventListener('click', this.clickHandler,true);
+  }
+
+  makeUnclickable(){
+    console.log('made UNclickable')
+    this.clickable = false;
+    canvas.removeEventListener('click', this.clickHandler,true);
+  }
+
+  clicker(evt) {
+    if (evt.pageX-rect.left>=this.x && evt.pageX-rect.left<=this.x+this.width && evt.pageY-rect.top>=this.y && evt.pageY-rect.top<=this.y+this.height){
+      this.color='rgb('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')';
+    }
+  }
+
+
+  yes(){
+    ctx.strokeStyle = 'black'
+    ctx.lineWidth = 1;
+  }
+  no(){
+    ctx.strokeStyle = 'red'
+    ctx.lineWidth = 3;
+  }
   draw(){
-    ctx.fillStyle='white';
-    ctx.fillRect(this.x,this.y,this.width,this.height);
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
     ctx.fillStyle = 'black';
     ctx.font = "30px Verdana";
-    ctx.fillText(this.text,this.x,this.y+this.height);
+    ctx.fillText(this.text, this.x, this.y + this.height);
+
+    this.clickable ? this.yes() : this.no();
+    
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x+this.width, this.y);
+    ctx.lineTo(this.x+this.width, this.y+this.height);
+    ctx.lineTo(this.x, this.y+this.height);
+    ctx.lineTo(this.x, this.y);
+    ctx.stroke();
   }
 }
--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+'use strict'
 var canvas =document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var rect=canvas.getBoundingClientRect();
 
 var CButtons=[];
-CButtons[0] = new CanvasButton(30,70,100,40,'test');
+CButtons[0] = new CanvasButton(30,70,100,40,'test','white',true);
+CButtons[1] = new CanvasButton(120,150,100,40,'test','white',true);
+CButtons[2] = new CanvasButton(400,300,100,40,'test','white',true);
+
+for (let i=0; i<CButtons.length; i++){
+  let ButtonName = 'B'+ (i+1);
+  document.getElementById(ButtonName).addEventListener('click',(evt)=> {
+    let buttonNr = parseInt(evt.target.id.slice(1,evt.target.length))-1;
+    CButtons[buttonNr].clickable ? CButtons[buttonNr].makeUnclickable() : CButtons[buttonNr].makeClickable();
+  })
+}
+
 
 function loop(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
